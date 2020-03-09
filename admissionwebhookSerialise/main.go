fc495bc11ec2b7bc7d03aee8cd12cbb908f486b5
Rajdeep Das <irajdeep94@gmail.com>
2020-03-09 20:28:40 +0100

    use go's native yaml lib

--- admissionwebhookSerialise/main.go
+++ admissionwebhookSerialise/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
+	yaml "gopkg.in/yaml.v2"
 	"k8s.io/api/admissionregistration/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/yaml"
 )
 
 func main() {
@@ -42,5 +42,28 @@ func main() {
 		panic(err.Error())
 	}
 	fmt.Printf("%s", string(data))
+	// 	inp := `metadata:
+	//   creationTimestamp: null
+	//   name: pod-validation-webhook-configration
+	// webhooks:
+	// - clientConfig:
+	//     url: https://127.0.0.1
+	//   name: pod-validation.booking-admission-controller.booking.com
+	//   rules:
+	//   - apiGroups:
+	//     - '*'
+	//     apiVersions:
+	//     - '*'
+	//     operations:
+	//     - CREATE
+	//     - UPDATE
+	//     resources:
+	// 	- pods`
 
+	// 	var config2 v1beta1.ValidatingWebhookConfiguration
+
+	// 	err = yaml.Unmarshall([]byte(inp), &config2)
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
 }
